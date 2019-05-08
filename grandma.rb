b231abb64952eef4247420c683de73862bafbf4a
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-08 18:39:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,17 +1,3 @@
-# Write a speak_to_grandma method.
-
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so)
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
-
 def speak_to_grandma (speak)
   if speak == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
@@ -20,7 +6,6 @@ def speak_to_grandma (speak)
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
-  # puts "I wrote in: #{speak}"
 end
 
 speak_to_grandma("HELLO")
