c5d1d57f5c2f07206c074a45bf008bfefd26f76e
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-08 18:36:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,11 @@
 
 def speak_to_grandma (speak)
   if speak == speak.upcase
-    puts "true"
+    puts "NO, NOT SINCE 1938!"
+  elsif speak == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   else
-    puts "false response"
+    puts "HUH?! SPEAK UP, SONNY!"
   end
   puts speak
 end
